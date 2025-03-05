576129e3b63043b8c15c3610e1dbc495ca282842
Tonny Fernandes <tonny.feh@gmail.com>
2025-03-05 18:16:13 -0300

    Update structs.c

--- structs.c
+++ structs.c
@@ -1,121 +1,29 @@
 #include <stdio.h>
 
-typedef struct sListaCompras{
-    int cachosBanana;
-    int macas;
-    int amaciante;
-    int esponja;
-}TListaCompras;
-typedef struct sTarefasDomesticas{
-    int banheiro;
-    int poeira;
-    int panelas;
-}TDomestica;
-typedef struct sFaculdade{
-    int estruturaDados;
-    int projetoIntegrador;
-}TFaculdade;
-typedef struct sProjetosPessoais{
-    int videosCurso;
-    int tarefasCurso;
-}TProjetos;
+typedef struct sBanda{
+    char nome[50];
+    char genero[50];
+    int numIntegrantes;
+    int posicao;
+}TBanda;
 
-typedef struct sListaTarefas{
-    TListaCompras compras;
-    TDomestica domestica;
-    TFaculdade faculdade;
-    TProjetos projetos;
-}TListaTarefas;
-
-
-void conferir_itemLista(int item){
-    if(item==0){
-        printf("Feito\n");
-    }
-    else{
-        printf("Pendente\n");
-    }
+void limparBuffer(){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
 
-void listaCompleta(TListaTarefas lista){
-    printf("Lista de Compras: \n\t Cachos de banana necessários: %d | ", lista.compras.cachosBanana);
-        conferir_itemLista(lista.compras.cachosBanana);
-    
-    printf("\n");
+void rankearBandas(TBanda bandas[]){
+    printf("Banda 1: %s", bandas[0].nome);
 }
 
-int main() {
-    TListaTarefas lista={
-        .compras={2, 5, 1, 1},
-            /*
-                Tradução:
-                    dois cachos de banana para comprar
-                    cinco maçãs para comprar
-                    uma amaciante e uma esponja para comprar
-                Caso um desses valores se iguale a zero, a tarefa será dada como "feita" pelo programa
-            */
-        .domestica={1, 1, 1},
-            /*
-                Tradução:
-                    Ainda é necessário limpar o banheiro, tirar a poeira da casa e lavar as panelas
-                Caso um desses valores se iguale a zero, a tarefa será dada como "feita" pelo programa
-            */
-        .faculdade={1, 1},
-            /*
-                Tradução
-                    Há atividades a serem feitas nas disciplinas de Estrutura de Dados e Projeto Integrador
-                Caso um desses valores se iguale a zero, a tarefa será dada como "feita" pelo programa
-            */
-        .projetos={2, 4},
-            /*
-                Tradução:
-                    Dois videos do curso para ver
-                    Quatro tarefas do curso para fazer
-                Caso um desses valores se iguale a zero, a tarefa será dada como "feita" pelo programa
-            */
-    };
-    int escolha;
+int main(){
+    TBanda listaBanda[5];
     
-    do{
-        printf("Digite 0 para fechar o programa\n");
-        printf("Digite 1 para ver a lista completa\n");
-        printf("Digite 2 para ver a lista de compras\n");
-        printf("Digite 3 para ver a lista de tarefas domésticas\n");
-        printf("Digite 4 para ver a lista da faculdade\n");
-        printf("Digite 5 para ver a lista de projetos pessoais\n");
-        scanf("%d", &escolha);
-        
-        printf("\n");
-        
-        switch(escolha){
-            case 0:
-                printf("Programa fechado!\n");
-                break;
-            
-            case 1:
-                listaCompleta(lista);
-                break;
-            
-            case 2:
-                // função listaCompras();
-                break;
-            
-            case 3:
-                // função listaDomestica();
-                break;
-            
-            case 4:
-                // função listaFaculdade();
-                break;
-            
-            case 5:
-                // função listaProjetos();
-                break;
-            
-            default:
-                printf("Escolha inválida, tente novamente\n\n");
-        }
-    }while(escolha!=0);
-
+    for(int i=0; i<5; i++){
+        printf("Digite o nome da %da banda\n", i+1);
+        scanf("%[^\n]", &listaBanda[i]);
+        limparBuffer();
+    }
+    rankearBandas(listaBanda[]);
     return 0;
 }
